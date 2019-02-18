c978055fca50096e442fc2166e7b0a5cbe389923
anmolsaxena10 <anmol.saxena10@gmail.com>
2019-02-18 17:44:26 +0530

    filtering and pagination completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,15 +9,48 @@ import {WineService} from '../wine.service';
 export class ListComponent implements OnInit {
 
   public wineList;
-
+  public curr_page = 1;
+  public country = '';
+  public province = '';
+  public region_1 = '';
+  public region_2 = '';
+  public page = this.curr_page;
+  public total_pages;
   constructor(private wineService: WineService) {
 
   }
 
   ngOnInit() {
+    this.getPage('curr');
+  }
+
+  public getPage(to){
     const that = this;
-    this.wineService.getWineList({'format': 'json', 'page': '1'}).subscribe(function(wines){
-      that.wineList = wines;
+    var params = {
+      'format': 'json',
+    };
+
+    if(to=='next'){
+      this.page=Math.min(this.curr_page+1, this.total_pages);
+      this.curr_page = this.page;
+    }
+    else if(to=='prev'){
+      this.page=Math.max(1, this.curr_page-1);
+      this.curr_page = this.page;
+    }
+
+    console.log(this.page);
+    params['page'] = this.page;
+    if(this.country != '') params['country'] = this.country;
+    if(this.province != '') params['province'] = this.province;
+    if(this.region_1 != '') params['region_1'] = this.region_1;
+    if(this.region_2 != '') params['region_2'] = this.region_2;
+
+    console.log(params);
+
+    this.wineService.getWineList(params).subscribe(function(wines){
+      that.wineList = wines.results;
+      that.total_pages = Math.ceil(wines.count/10);
       console.log(that.wineList);
     });
   }
